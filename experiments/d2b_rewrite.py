aa04d7cc4a5fdb0ebdd6e33c6b5c58a121d7ef1f
Xiyue Wang <harrietwang122@gmail.com>
2026-09-06 22:31:47 +0800

    Update d2b_rewrite.py

--- experiments/d2b_rewrite.py
+++ experiments/d2b_rewrite.py
@@ -4,14 +4,39 @@ from src.tools.get_preauthorisation import (
     get_preauthorisation,
     get_preauthorisation_v2,
 )
-case = {
-    "member_id": "M-2214",
-    "procedure_code": "62480",
-    "date_of_service": "2026-09-01",
-}
+cases = [
+    {
+        "name": "valid",
+        "member_id": "M-2214",
+        "procedure_code": "62480",
+        "date_of_service": "2026-09-01",
+    },
+    {
+        "name": "expired",
+        "member_id": "M-6118",
+        "procedure_code": "29881",
+        "date_of_service": "2026-09-01",
+    },
+    {
+        "name": "missing",
+        "member_id": "M-9999",
+        "procedure_code": "99999",
+        "date_of_service": "2026-09-01",
+    },
+    {
+        "name": "invalid_date",
+        "member_id": "M-2214",
+        "procedure_code": "62480",
+        "date_of_service": "not-a-date",
+    },
+]
 
-v1_result = get_preauthorisation(**case)
-v2_result = get_preauthorisation_v2(**case)
+for case in cases:
+    name = case.pop("name")
 
-print("V1:", json.dumps(v1_result))
-print("V2:", json.dumps(v2_result))
+    v1_result = get_preauthorisation(**case)
+    v2_result = get_preauthorisation_v2(**case)
+
+    print(f"\nCASE: {name}")
+    print("V1:", json.dumps(v1_result))
+    print("V2:", json.dumps(v2_result))
